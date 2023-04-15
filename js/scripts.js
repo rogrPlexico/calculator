a5c415ef592d5e766d9f73f648cb12fe0745be83
rogrPlexico <96746687+rogrPlexico@users.noreply.github.com>
2023-04-15 11:15:45 -0400

    add first stage of structure and css (for later reference)

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+
+function operate() {
+    let x;
+    let y;
+    let operator;
+
+    operator(x, y);
+}
+
+
+function add(x, y) {
+    return x + y;
+} 
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    return x / y;
+}
+
+
